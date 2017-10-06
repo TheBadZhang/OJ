631d35f282660070633520fcd5578c261a49f046
TheBadZhang <thebadzhang@gmail.com>
2017-10-06 18:12:05 +0800

    171006

---
+++ openjudge/01/06/10.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+
+int main () {
+
+	char *R = new char [300];
+	char *A = new char [300];
+	char *B = new char [300];
+	for (int a = 0; a < 300; a += 1) {
+		R [a] = 0;
+		A [a] = 0;
+		B [a] = 0;
+	}
+	R [300] = 0;
+	std::string ss1;
+	std::string ss2;
+	std::cin >> ss1 >> ss2;
+	int lena = ss1.length(), lenb = ss2.length();
+	int maxsize = lena > lenb ? lena : lenb;
+	for (int a = 0; a < lena; a += 1) {
+		A [lena-a-1] = ss1 [a] - 48;
+	}
+	for (int a = 0; a < lenb; a += 1) {
+		B [lenb-a-1] = ss2 [a] - 48;
+	}
+
+	for (int a = 0; a < maxsize; a += 1) {
+		R [a] += A [a]+ B [a];
+		if (R [a] >= 10) {
+			R [a+1] += R [a] / 10;
+			R [a] %= 10;
+		}
+	}
+	if (R [maxsize] > 0) {
+		maxsize += 1;
+	}
+	bool flag = false;
+	for (int a = 0; a < maxsize; a += 1) {
+		if (!flag && R [maxsize-a-1] != 0) {
+			flag = true;
+		}
+		if (flag) {
+			printf ("%d",R [maxsize-a-1]);
+		}
+	}
+	if (!flag && R [0] == 0) {
+		printf ("0");
+	}
+
+
+
+	return 0;
+}
---
+++ openjudge/01/06/11.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+
+int main () {
+
+	char *R = new char [300];
+	char *A = new char [300];
+	char *B = new char [300];
+	for (int a = 0; a < 300; a += 1) {
+		R [a] = 0;
+		A [a] = 0;
+		B [a] = 0;
+	}
+	R [300] = 0;
+	std::string ss1;
+	std::string ss2;
+	std::cin >> ss1 >> ss2;
+	int lena = ss1.length(), lenb = ss2.length();
+	int maxsize = lena > lenb ? lena : lenb;
+	for (int a = 0; a < lena; a += 1) {
+		A [lena-a-1] = ss1 [a] - 48;
+	}
+	for (int a = 0; a < lenb; a += 1) {
+		B [lenb-a-1] = ss2 [a] - 48;
+	}
+
+	for (int a = 0; a < maxsize; a += 1) {
+		// 下面三处代码与10.cpp有所不同，其他相同
+		R [a] += A [a] - B [a];
+		if (R [a] < 0) {
+			R [a+1] -= 1;
+			R [a] += 10;
+		}
+	}
+	if (R [maxsize] > 0) {
+		maxsize += 1;
+	}
+	bool flag = false;
+	for (int a = 0; a < maxsize; a += 1) {
+		if (!flag && R [maxsize-a-1] != 0) {
+			flag = true;
+		}
+		if (flag) {
+			printf ("%d",R [maxsize-a-1]);
+		}
+	}
+	if (!flag && R [0] == 0) {
+		printf ("0");
+	}
+
+
+
+	return 0;
+}
